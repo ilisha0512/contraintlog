295cd03952f1953a37d49751d05d333b783c9298
ilisha0512 <68852416+ilisha0512@users.noreply.github.com>
2020-08-24 16:47:40 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# contraintlog
+# angryBirdsStage2
+Angry Birds stage 2 with Class Inheritance and Images
---
+++ slingshot.js
@@ -0,0 +1,18 @@
+class slingshot {
+    constructor (bodyA, bodyB) {
+      var options ={
+          bodyA: bodyA,
+          bodyB: bodyB,
+          stiffness: 1,
+          length: 200,
+      }
+      this.chain = Constraint.create(options);
+      World.add(world, this.chain);
+    }
+    display(){
+        var pointA = this.chain.bodyA.position;
+        var pointB = this.chain.bodyB.position;
+        stroke("gray");
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+}
